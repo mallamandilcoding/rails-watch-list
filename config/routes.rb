346e317f77c953685b1bf0fcd69ac35221668b38
Mandil Malla <mallamandilcoding@gmail.com>
2023-02-17 16:58:27 +0900

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # root to: 'lists#index'
+  root to: 'pages#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
